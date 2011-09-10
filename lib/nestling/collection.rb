93f8d2b1e0e5877c7378cba1b84a050918f1a66e
Tobias Svensson <tob@tobiassvensson.co.uk>
2011-09-10 23:49:57 +0100

    Add type assignment to collection objects.

--- lib/nestling/collection.rb
+++ lib/nestling/collection.rb
@@ -1,12 +1,13 @@
 module Nestling
   class Collection < Array
-    attr_accessor :results, :start, :session_id
+    attr_accessor :results, :start, :session_id, :type
 
     def initialize(options = {}, *args)
       options = Nestling::Hash.new(options) # symbolize_keys!
       @results    = options[:results] || options[:total]
       @start      = options[:start]
       @session_id = options[:session_id]
+      @type       = options[:type]
       super *args
     end
   end
